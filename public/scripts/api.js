f7b5ea1e026f84ead5c3770af48b3a4d77601506
Joe-Pena <joepena@protonmail.com>
2018-09-05 16:34:58 -0600

    Added from handler in noteful.js and api update method

--- public/scripts/api.js
+++ public/scripts/api.js
@@ -3,22 +3,33 @@
 
 const api = {
 
-  search: function (query, callback) {
-    $.ajax({
-      type: 'GET',
-      url: '/api/notes/',
-      dataType: 'json',
-      data: query,
-      success: callback
-    });
-  },
+    search: function (query, callback) {
+        $.ajax({
+            type: 'GET',
+            url: '/api/notes/',
+            dataType: 'json',
+            data: query,
+            success: callback
+        });
+    },
 
-  details: function (id, callback) {
-    $.ajax({
-      type: 'GET',
-      dataType: 'json',
-      url: `/api/notes/${id}`,
-      success: callback
-    });
-  }
+    details: function (id, callback) {
+        $.ajax({
+            type: 'GET',
+            dataType: 'json',
+            url: `/api/notes/${id}`,
+            success: callback
+        });
+    },
+
+    update: function (id, obj, callback) {
+        $.ajax({
+            type: 'PUT',
+            url: `/api/notes/${id}`,
+            contentType: 'application/json',
+            dataType: 'json',
+            data: JSON.stringify(obj),
+            success: callback
+        });
+    }
 };
